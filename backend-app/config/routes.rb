e7b1b15ef92486607f8d1e18acecafa13eac59a9
Andrew Patterson <andrew.patterson.3001@gmail.com>
2016-03-05 20:14:13 -0800

    renders classrooms#index correctly, although I will not be using the view templates for my API.

--- backend-app/config/routes.rb
+++ backend-app/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'classooms#index'
+  root 'classrooms#index'
 
   resources :classrooms do
     resources :students
