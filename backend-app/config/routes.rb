dffa80d87cd9f7a92dbd669434c568c5a425f137
Andrew Patterson <andrew.patterson.3001@gmail.com>
2016-03-08 11:32:51 -0800

    add welcome page to see if that helps with buildpack setting

--- backend-app/config/routes.rb
+++ backend-app/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'classrooms#index'
+  root 'welcome#index'
 
   resources :classrooms do
     resources :students
