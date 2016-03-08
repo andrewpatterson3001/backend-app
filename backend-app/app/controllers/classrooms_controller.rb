735991ec2d8b73768f70da7b17085182336834ba
Andrew Patterson <andrew.patterson.3001@gmail.com>
2016-03-08 10:23:00 -0800

    render json for classrooms controller using 'render json: @instance'

--- backend-app/app/controllers/classrooms_controller.rb
+++ backend-app/app/controllers/classrooms_controller.rb
@@ -1,9 +1,11 @@
 class ClassroomsController < ApplicationController
   def index
     @classrooms = Classroom.all
+    render json: @classrooms
   end
 
   def show
     @classroom = Classroom.find_by_id(params[:id])
+    render json: @classroom
   end
 end
