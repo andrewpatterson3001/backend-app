572cf02e41284a4b31868314cf917e7fa8baa626
Andrew Patterson <andrew.patterson.3001@gmail.com>
2016-03-14 14:02:43 -0700

    move everything up one level and remove unnecessary folder. this should also fix heroku deployment problem.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,7 +7,6 @@ class StudentsController < ApplicationController
 
   def show
     #list the email and classroom of a student as well as how many points accumulated
-    p params
     @student = Student.where(classroom_id: params[:classroom_id], id: params[:id])
     render json: @student
   end
