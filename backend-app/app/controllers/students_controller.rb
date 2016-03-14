45df34a5cd1403915faba7cd8d99fb6fb0c5cbaa
Andrew Patterson <andrew.patterson.3001@gmail.com>
2016-03-14 13:47:42 -0700

    add index and show actions to the Student Controller

--- backend-app/app/controllers/students_controller.rb
+++ backend-app/app/controllers/students_controller.rb
@@ -1,9 +1,14 @@
 class StudentsController < ApplicationController
   def index
     #list students within a class
+    @students = Student.where(classroom_id: params[:classroom_id])
+    render json: @students
   end
 
   def show
     #list the email and classroom of a student as well as how many points accumulated
+    p params
+    @student = Student.where(classroom_id: params[:classroom_id], id: params[:id])
+    render json: @student
   end
 end
